fa9301dbcc64a05414a38f7d20fed3ef8925de6c
Simone <150595274+simonetimi@users.noreply.github.com>
2024-11-22 21:05:03 +0100

    adjust transparences, shadows and background gradient

--- components/CharactersSelector.tsx
+++ components/CharactersSelector.tsx
@@ -61,9 +61,9 @@ const CharactersSelector = () => {
 
   return (
     <div className="lg:w-[500px] w-11/12 flex flex-col items-center gap-4">
-      <Card>
+      <Card className="bg-white/20 backdrop-blur-lg border-slate-100/20 shadow-md">
         <CardHeader>
-          <CardTitle className="text-center">
+          <CardTitle className="text-center flex flex-col gap-1">
             <p>{selectedCharacter?.name}</p>
             <p>&#34;{selectedCharacter?.nickname}&#34;</p>
           </CardTitle>
--- components/Header.tsx
+++ components/Header.tsx
@@ -2,8 +2,13 @@ import React from "react";
 
 const Header = () => {
   return (
-    <header className="p-4 flex items-center border-b border-slate-200 h-[10vh]">
-      <h1 className="text-2xl">Identity V Character Randomizer</h1>
+    <header className="p-4 flex items-center border-b border-slate-100/20 h-[10vh] shadow bg-white/10 backdrop-blur-lg">
+      <h1 className="text-2xl">
+        <span className="font-bold">Identity V</span>{" "}
+        <span className="underline-offset-8 underline">
+          Character Randomizer
+        </span>
+      </h1>
     </header>
   );
 };
