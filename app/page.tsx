45a2475ab3e13eb4c3281666375cc789363d484f
Simone <150595274+simonetimi@users.noreply.github.com>
2024-11-23 17:20:49 +0100

    fix height for iOS devices

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import CharactersSelector from "@/components/CharactersSelector";
 
 export default function Home() {
   return (
-    <main className="flex flex-col justify-center items-center h-[90vh]">
+    <main className="flex flex-col justify-center items-center h-[90dvh]">
       <CharactersSelector />
     </main>
   );
--- components/Header.tsx
+++ components/Header.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Header = () => {
   return (
-    <header className="p-4 flex items-center border-b border-slate-100/20 h-[10vh] shadow bg-white/10 backdrop-blur-lg">
+    <header className="p-4 flex items-center border-b border-slate-100/20 h-[10dvh] shadow bg-white/10 backdrop-blur-lg">
       <h1 className="text-2xl">
         <span className="font-bold">Identity V</span>{" "}
         <span className="underline-offset-8 underline">
