752748074e596f616b711da0367e98c5fd2fccf9
Simone <150595274+simonetimi@users.noreply.github.com>
2024-11-24 17:38:54 +0100

    improve navigation so that the logo is now a link to the home page

--- components/Header.tsx
+++ components/Header.tsx
@@ -17,15 +17,17 @@ const Header = () => {
 
   return (
     <header className="sticky z-10 top-0 p-4 flex items-center border-b border-slate-100/20 h-[12dvh] shadow bg-white/10 backdrop-blur">
-      <div className="flex flex-col items-center">
-        <img src="/images/iv-logo.png" width="150" alt="identity v logo" />
-        <h2 className="text-white font-bold drop-shadow">
-          Character Randomizer
-        </h2>
-      </div>
+      <Link href="/">
+        <div className="flex flex-col items-center">
+          <img src="/images/iv-logo.png" width="150" alt="identity v logo" />
+          <h2 className="text-white font-bold drop-shadow">
+            Character Randomizer
+          </h2>
+        </div>
+      </Link>
       <Button className="ml-auto" asChild>
         <Link href={linkUrl}>
-          {pathname === "/" ? "Recent characters" : "Back"}
+          {pathname === "/" ? "Recent characters" : "Go back"}
         </Link>
       </Button>
     </header>
